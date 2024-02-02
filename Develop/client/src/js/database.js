c820e0731d2234e4fbf04078b3e882c06c60f55f
Madison <madie-v@live.com>
2024-02-01 20:07:41 -0600

    completed TODO's in database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const JateDb = await openDB('Jate', 1);
+  const tx = JateDb.transaction('Jate', 'readwrite');
+  const store = tx.objectStore('Jate');
+  const request = store.add({ JEST: content });
+  const result = await request;
+  console.log(`Data added to database!`, result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const JateDb = await openDB('Jate', 1);
+  const tx = JateDb.transaction('Jate', 'readonly');
+  const store = tx.objectStore('Jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result.value);
+  return result.value;
+};
 
 initdb();
