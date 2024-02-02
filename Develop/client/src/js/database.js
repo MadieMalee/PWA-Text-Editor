437e9cd6cddd8b2c37c2669723d33eeb98e509b5
Madison <madie-v@live.com>
2024-02-01 19:50:20 -0600

    completed TODO's on install.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,14 +1,14 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('Jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('Jate')) {
+        console.log('Jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('Jate', { keyPath: 'id', autoIncrement: true });
+      console.log('Jate database created');
     },
   });
 
